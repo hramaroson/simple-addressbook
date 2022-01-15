bd41d35daf924678ba034e694ee1770046fb9bf0
Herizo Ramaroson <39248388+hramaroson@users.noreply.github.com>
2022-01-15 19:48:28 +0300

    Use argparse to parse CLI arguments

--- addressbook.py
+++ addressbook.py
@@ -1,20 +1,44 @@
 #!/usr/bin/python3 
 import sys
+import argparse
+
+class Contact:
+    def __init__(self,firstname, lastname, address="", phone="", email=""):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.address = address
+        self.phone = phone
+        self.email = email
+
 class AddressBook:
+    _addressbook = []
     def print_help():
         print("A simple Address book CLI program\n"
-        "Usage: adressbook [--help]\n\n"
-        "Available commands:\n"
-        "\tAdd\t")
+        "Usage: adressbook.py [--help] <command> [<args>]\n\n"
+        "Available commands:\n"             
+        "\tadd\tAdd a contact to the adress book")
 
 def main():
     addressbook = AddressBook
-    _argv = sys.argv
 
-    #if no argument given or asking for help
-    if len(_argv) < 2 or _argv[1].lower() == "help" or "--help":
-        addressbook.print_help()
-        return
+    parser = argparse.ArgumentParser(description="A Simple Python CLI program to manage Adress book")
+    parser.add_argument("command", help="Command to execute", type=str.lower, choices = ["add"])
+    parser.add_argument("-fn", "--firstname", type= str.lower, help="Contact first name")
+    args = parser.parse_args()
+
+    #Add a contact to the address
+    if args.command == "add": 
+        #Check if contact firstname is given
+        _firstname = ""
+        if args.firstname:
+            if len(args.firstname) > 0:
+                _firstname = args.firstname
+        
+        if len(_firstname)<1:
+            print("{appname}:'{command}': Contact first name is required. See '{appname} help'"
+                .format(appname=sys.argv[0], command=args.command))
+            return
+        
 
 if __name__ == "__main__":
     main()
